b550708db0577867c6f865a4b9330fcbcfb6e812
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-07-06 15:42:23 -0400

    added code to the LoginForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,33 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 
- // Import Mutations 
+// Import Mutations
 
+// Import LOGIN_USER from Mutations.js
 
- // 
+// Import Auth from Auth.js
+
+// Set up LoginForm
+const LoginForm = () => {
+  const handleInputChange = (event) => {};
+  const handleFormSubmit = async (event) => {};
+
+  // Check if the form has everything accurately listed
+  const form = event.currentTarget;
+  if (form.checkValidity() === false) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  try{
+    const {data} = await loginUser({
+        variables: {...userFormData}
+    });
+
+    console.log(data);
+    Auth.login(data.login.token);
+  } catch (err){}
+};
+
+// Return Form information here
+return (<></>)
---
+++ client/src/pages/Resource.js
@@ -0,0 +1,1 @@
+// Setting up a resource page - this would be where we have a list of city information and links
