4762ebbabd85711ec8f87d1a1e4c26bab54bfeaa
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-07-07 11:45:00 -0400

    setting up Profile.js in the pages subfolder

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,19 @@
+// Import React
+import React from "react";
+
+// Import Navigate and useParams
+import { Navigate, useParams } from "react-router-dom";
+
+// Import useQuery and useMutation
+import { useQuery, useMutation } from "@apollo/client";
+
+// TODO: KAJIAM - Import queries we'll need!
+
+// Import Auth
+import Auth from "../utils/auth";
+
+const Profile = (props) => {
+  const { username: userParam } = useParams();
+
+  // TODO - Queries to go here?
+};
