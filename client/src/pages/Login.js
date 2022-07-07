8e072ef857fa9d577006d2b2a0a6dd9667b98c6e
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-07-07 11:54:48 -0400

    moved login and signup to pages - also cleaned up login.js

---
+++ client/src/pages/Login.js
@@ -0,0 +1,14 @@
+// Import React
+import React, { useState } from "react";
+
+// Import useMutation
+import { useMutation } from "@apollo/client";
+
+// TODO: KAJIAM - Import mutations from utils/mutations
+
+// Import LOGIN_USER from Mutations.js
+
+// Import Auth
+import Auth from "../utils/auth";
+
+export default LoginForm;
