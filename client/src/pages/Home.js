929abd0beef7c4e9bf969bfa8c1031d12e0d5bfd
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-07-07 11:40:33 -0400

    finished boilerplate code for Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,12 +3,36 @@ import React from "react";
 
 // Import Auth
 import Auth from "../utils/auth";
+
+// Import Footer
+import Footer from "../components/Footer";
+
+import Header from "../components/Header";
+
 // Import useQuery
 import { useQuery } from "@apollo/client";
-// TODO: KAIJAM - Import the queries we'll need from utils/queries
+// TODO: KAJIAM - Import the queries we'll need from utils/queries
 
 // Set up Home function
 const Home = () => {
   // Set up a constant for being loggedIn
   const loggedIn = Auth.loggedIn();
+
+  return (
+    <main>
+      <div className="flex-row justify-space-between">
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            {/* Add in Header */}
+            <Header />
+            {/* Add in Footer */}
+            <Footer />
+          </div>
+        )}
+      </div>
+    </main>
+  );
 };
+
+// Export Home
+export default Home;
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-//TODO: Kaijam
+//TODO: KAJIAM
 
 // Import GQL from Apollo
 
