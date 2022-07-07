380d2c2db7840f7be0dd248949bf5f6a7195cc9d
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-07-07 19:55:44 -0400

    added in the rest of Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -14,7 +14,76 @@ import Auth from "../utils/auth";
 const Login = (props) => {
   // Set up login formState
   const [formState, setFormState] = useState({ email: "", password: "" });
-  // const [login, { error }] = useMutation(LOGIN_USER);
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  // Update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // Submit Form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // Clear form values
+    setFormState({
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <main className="flex-row justify-center mb-4">
+      <div className="col-12 col-md-6">
+        <div className="card">
+          <h4 className="card-header">Login</h4>
+          <div className="card-body">
+            <form onSubmit={handleFormSubmit}>
+              {/* Set up form input */}
+              <input
+                className="form-input"
+                // Email
+                placeholder="Your email"
+                name="email"
+                type="email"
+                id="email"
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <input
+                className="form-input"
+                placeholder="******"
+                name="password"
+                type="password"
+                id="password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <button className="btn d-block w-100" type="submit">
+                Submit
+              </button>
+            </form>
+            {error && <div>Login failed!</div>}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
+// Export Login
 export default Login;
