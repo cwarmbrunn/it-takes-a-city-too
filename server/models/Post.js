55b97600818c13f7479d71555e8cb9ba2dbbcb8f
Matt11711 <matthew.d.allen97@gmail.com>
2022-07-07 20:05:17 -0400

    updated post.js

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Comment.js');
+const commentSchema = require('./Comment.js');
 const dateFormat = require('../utils/dateFormat');
 
 const postSchema = new Schema(
@@ -28,8 +28,8 @@ const postSchema = new Schema(
   }
 );
 
-postSchema.virtual('reactionCount').get(function() {
-  return this.reactions.length;
+postSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
 });
 
 const Post = model('Post', postSchema);
