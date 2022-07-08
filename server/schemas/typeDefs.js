1e7fcd3a6e7ab2cbd889ecc71bfafa72c3097a58
Kaijam Jeffers <faceless128@gmail.com>
2022-07-07 21:25:41 -0400

    revert resolvers/typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,35 +1,49 @@
-// import the gql tagged template function
-const { gql } = require('apollo-server-express');
+// Import the gql tagged template function
+const { gql } = require("apollo-server-express");
 
-// create our typeDefs
+// Create our typeDefs
 const typeDefs = gql`
   type User {
     _id: ID
     username: String
     email: String
-    postCount: Int
+    friendCount: Int
     posts: [Post]
+    friends: [User]
   }
-
   type Post {
     _id: ID
-    title: String
-    content: String
+    postText: String
+    createdAt: String
     username: String
-    replies: [Reply]
-  }                    
-
-  type Replies {
+    commentCount: Int
+    comments: [Comment]
+  }
+  type Comment {
     _id: ID
-    replyBody: String
+    commentBody: String
+    createdAt: String
     username: String
   }
-
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    posts(username: String): [Post]
+    post(_id: ID!): Post
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addPost(postText: String!): Post
+    addComment(postId: ID!, commentBody: String!): Post
+    addFriend(friendId: ID!): User
+  }
   type Auth {
     token: ID!
     user: User
   }
 `;
 
-// export the typeDefs
+// Export the typeDefs
 module.exports = typeDefs;
