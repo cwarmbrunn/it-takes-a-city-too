35231da5efa0c7a8015ba40c7be6d15294c01423
Kaijam Jeffers <faceless128@gmail.com>
2022-07-10 19:00:19 -0400

    added user seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -3,6 +3,7 @@ const { User, Post, Tag } = require("../models");
 
 db.once("open", async () => {
   await Tag.deleteMany();
+  await User.deleteMany({});
 
   const categories = await Category.insertMany([
     { tagBody: "Shelter" },
@@ -13,4 +14,42 @@ db.once("open", async () => {
   ]);
 
   console.log("tags seeded");
+
+  await User.create([
+    [
+      {
+        "username" : "Christina",
+        "email" : "christina@ittakesacity.org",
+        "password" : "test1234", 
+        "posts" : []
+      },
+      {
+        "username" : "Madison",
+        "email" : "madison@ittakesacity.org",
+        "password" : "test1234", 
+        "posts" : []
+      },
+      {
+        "username" : "Kaijam",
+        "email" : "kaijam@ittakesacity.org",
+        "password" : "test1234", 
+        "posts" : []
+      },
+      {
+        "username" : "Konner",
+        "email" : "konner@ittakesacity.org",
+        "password" : "test1234", 
+        "posts" : []
+      },
+      {
+        "username" : "Matt",
+        "email" : "matt@ittakesacity.org",
+        "password" : "test1234", 
+        "posts" : []
+     }
+    ]
+  ]);
+
+  console.log("tags seeded");
+
 });
