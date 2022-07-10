fcb7779f7a037c5c68c8534dc573d4bbef29b1f7
Matt11711 <matthew.d.allen97@gmail.com>
2022-07-09 20:10:16 -0400

    updated models

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,38 +1,42 @@
-const { Schema, model } = require('mongoose');
-const commentSchema = require('./Comment.js');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require("mongoose");
+const commentSchema = require("./Comment.js");
+const dateFormat = require("../utils/dateFormat");
 
 const postSchema = new Schema(
   {
     postText: {
       type: String,
-      required: 'You need to leave a thought!',
+      required: "You need to leave a description!",
       minlength: 1,
-      maxlength: 280
+      maxlength: 280,
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: timestamp => dateFormat(timestamp)
+      get: (timestamp) => dateFormat(timestamp),
     },
     username: {
       type: String,
-      required: true
+      required: true,
     },
-    tags: [tagSchema],
-    comments: [commentSchema]
+    tags: [String],
+    location: {
+      type: String,
+      Required: true,
+    },
+    comments: [commentSchema],
   },
   {
     toJSON: {
-      getters: true
-    }
+      getters: true,
+    },
   }
 );
 
-postSchema.virtual('commentCount').get(function() {
+postSchema.virtual("commentCount").get(function () {
   return this.comments.length;
 });
 
-const Post = model('Post', postSchema);
+const Post = model("Post", postSchema);
 
 module.exports = Post;
