ad2e586e9347094b89911543094e8bc20ecb6a14
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-07-11 16:28:05 -0400

    added test CSS

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
-          <h1> It Takes a City Too</h1>
+          <h1 className="text-decoration-none"> It Takes a City Too</h1>
         </Link>
         <nav className="text-center">
           {/* Set up functionality if user is logged in */}
