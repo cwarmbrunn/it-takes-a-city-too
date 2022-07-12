30056113f6d9c47f867949334e3a989886af5a0a
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-07-11 20:55:50 -0400

    added header and footer content

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className=" text-light text-center w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy; 2022 by Group 4</div>
+      <div className="container">&copy; 2022 by Group 🍀</div>
     </footer>
   );
 };
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -15,13 +15,7 @@ const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link to="/" className="text-decoration-none">
-          <h1 className="text-light flex-row align-center">
-            {" "}
-            It Takes a City Too
-          </h1>
-        </Link>
-        <nav className="flex items-center justify-between">
+        <nav className="flex items-center justify-between flex-wrap bg-gray-600 p-6">
           {/* Set up functionality if user is logged in */}
           {Auth.loggedIn() ? (
             <>
@@ -40,11 +34,31 @@ const Header = () => {
           ) : (
             <>
               {/* If the user is not logged in - allow them to only see the following */}
-
-              {/* Enables the user to login with existing credentials*/}
-              <Link to="/login">Login</Link>
-              {/* Enables the user to sign up with new credentials */}
-              <Link to="/signup">Signup</Link>
+              <nav className="navbar navbar-expand-lg">
+                <a className="text-light text-decoration-none" href="/">
+                  It Takes A City Too
+                </a>
+                <ul className="navbar-nav">
+                  <li className="nav-item active">
+                    <a
+                      className="nav-link text-decoration-none text-light"
+                      href="/login"
+                    >
+                      {/* Enables the user to login with existing credentials */}
+                      Login
+                    </a>
+                  </li>
+                  {/* Enables the user to sign up with existing credentials */}
+                  <li className="nav-item active">
+                    <a
+                      className="nav-link text-decoration-none text-light"
+                      href="/signup"
+                    >
+                      Signup
+                    </a>
+                  </li>
+                </ul>
+              </nav>
             </>
           )}
         </nav>
