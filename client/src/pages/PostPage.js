f86301fdaa269caf0d72ef267bd6a24a9310b3a3
Madison Kendall <lilyzetakovic@gmail.com>
2022-07-13 16:30:16 -0600

    add state for addPost

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -7,23 +7,56 @@ import { useMutation } from "@apollo/client";
 // Import LOGIN_USER from Mutations.js
 // import { LOGIN_USER } from "../utils/mutations";
 
+// Import ADD_POST from Mutations.js
+import { ADD_POST } from "../utils/mutations";
+
 // Import Auth
 import Auth from "../utils/auth";
 
 const PostPage = () => {
- function handlePostItem(){
-    //Madison:hit api route to post by grabbing info from form
- }
+
+  const [formState, setFormState] = useState({
+    name: "",
+    address: "",
+    tags: "",
+  });
+
+  const [addPost, { error }] = useMutation(ADD_POST);
+
+  // update the state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // Submit Post
+ const handlePostItem = async (event) => {
+   event.preventDefault();
+
+   try {
+     const { data } = await addPost({ variables: {...formState }});
+     Auth.login(data.addPost.token);
+   } catch (e) {
+     console.log(e);
+   }
+ };
+
  return (
-    //Madison: style this page
     <main className="flex-row justify-center mb-4">
       <div className="col-12 col-md-6">
         <div className="card">
           <h4 className="card-header">Add a Resource</h4>
           <div className="card-body">
             <form onSubmit={handlePostItem}>
+                {/* Form Input - Enter Location Name */}
                 <input className="form-input" placeholder="Enter Location Name"></input>
+                {/* Form Input - Enter Location Address */}
                 <input className="form-input" placeholder="Enter Address"></input>
+                {/* Form Input - Emter Tags */}
                 <input className="form-input" placeholder="Enter Tags"></input>
                 {/* Submit new Post button */}
                 <button className="btn d-block w-100" type="submit" style= {{ background: "orange" }}>
