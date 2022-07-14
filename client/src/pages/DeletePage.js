e4910d95b46a4cce644d31b823f475b32520fc5e
Madison Kendall <lilyzetakovic@gmail.com>
2022-07-14 14:27:48 -0600

    add imports into DeletePage.js

---
+++ client/src/pages/DeletePage.js
@@ -0,0 +1,11 @@
+// Import React
+import React, { useState } from "react";
+
+// Import useMutation
+import { useMutation } from "@apollo/client";
+
+// Import DELETE_POST from Mutations.js
+import { DELETE_POST } from "../utils/mutations";
+
+// Import Auth
+import Auth from "../utils/auth";
