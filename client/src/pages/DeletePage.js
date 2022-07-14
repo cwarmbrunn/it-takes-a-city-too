8820f073f224f7d77732579a2a8fc1489b700af4
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-07-14 16:57:45 -0400

    resolved bug on the delete post issue

--- client/src/pages/DeletePage.js
+++ client/src/pages/DeletePage.js
@@ -11,11 +11,8 @@ import { DELETE_POST } from "../utils/mutations";
 import Auth from "../utils/auth";
 
 const DeletePage = () => {
-
   const [formState, setFormState] = useState({
-    name: "",
-    address: "",
-    tags: "",
+    _id: "",
   });
 
   const [deletePost, { error }] = useMutation(DELETE_POST);
@@ -31,66 +28,51 @@ const DeletePage = () => {
   };
 
   // Submit Post
- const handlePostItem = async (event) => {
-   event.preventDefault();
+  const handlePostItem = async (event) => {
+    event.preventDefault();
 
-   try {
-     const { data } = await deletePost({ variables: {...formState }});
-     Auth.login(data.addPost.token);
-   } catch (e) {
-     console.log(e);
-   }
- };
+    try {
+      const { data } = await deletePost({ variables: { ...formState } });
+      Auth.login(data.addPost.token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
- return (
+  return (
     <main className="flex-row justify-center mb-4">
       <div className="col-12 col-md-6">
         <div className="card">
           <h4 className="card-header">Delete Post</h4>
           <div className="card-body">
             <form onSubmit={handlePostItem}>
-                {/* Form Input - Enter Location Address */}
-                <input
+              {/* Form Input - Enter Location Address */}
+              <input
                 className="form-input"
-                placeholder="Enter Location Name"
-                name="name"
+                placeholder="Enter Post ID"
+                name="_id"
                 type="text"
-                id="locationName"
+                id="_id"
                 value={formState.name}
                 onChange={handleChange}
-                />
-                {/* Form Input - Enter Location Address */}
-                <input
-                className="form-input"
-                placeholder="Enter Address"
-                name="address"
-                type="text"
-                id="locationAddress"
-                value={formState.address}
-                onChange={handleChange}
-                />
-                {/* Form Input - Enter Tags */}
-                <input
-                className="form-input"
-                placeholder="Enter Tags"
-                name="tags"
-                type="text"
-                id="tags"
-                value={formState.tags}
-                onChange={handleChange}
-                />
-                {/* Submit newly edited Post button */}
-                <button className="btn d-block w-100" type="submit" style= {{ background: "orange" }}>
+              />
+
+              {/* Submit newly edited Post button */}
+              <button
+                className="btn d-block w-100"
+                type="submit"
+                style={{ background: "orange" }}
+              >
                 Submit
-                </button>
-             </form>
-             {error && <div className="text-danger">Could not delete post!</div>}
+              </button>
+            </form>
+            {error && <div className="text-danger">Could not delete post!</div>}
           </div>
         </div>
       </div>
     </main>
   );
-}
+};
 
 // export DeletePage
 export default DeletePage;
